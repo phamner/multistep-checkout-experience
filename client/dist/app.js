251b140fd47ce4c75057d446a568373e5561d033
Philip Hamner <philiphamner@gmail.com>
2020-04-22 15:07:44 -0700

    Add first react component that renders a button

---
+++ client/dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/app.js'
+var App = function App() {
+  return 'HELLO FROM REACTTTT';
+};
+
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('app'));
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,24 @@
 
 // import React from 'react';
 // import ReactDOM from 'react-dom';
+// import App from './components/app.js'
 
-ReactDOM.render(<h1>HELLO FROM REACT there </h1>, document.getElementById('app'));
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return(
+      //The divs below will hold my app
+      <div>
+        <h1>the app lives here</h1>
+        <button>Click me sucka</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
